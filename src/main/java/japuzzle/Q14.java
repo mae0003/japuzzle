ef471fc8d40267fa7c0ccc2a717a251e9d6b8b89
mae0003 <mae0003@gmail.com>
2015-12-12 15:06:17 +0900

    updated

--- src/main/java/japuzzle/Q14.java
+++ src/main/java/japuzzle/Q14.java
@@ -1,9 +1,13 @@
 package japuzzle;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Q14 extends Question {
+	
+	private int maxCount = 0;
+	
 	// 以下の 34 単語でしりとりをした場合、最大何回続き、それが何通りあるか プログラムを使って求めなさい。
 	// 同じ単語は１度しか使えません
 	static List<String> words = Arrays.asList(
@@ -15,6 +19,24 @@ public class Q14 extends Question {
 
 	@Override
 	public void solve() {
+		words.forEach( word -> {
+			appendNextWord(word, new ArrayList<String>());
+		});
+	}
 
+	private void appendNextWord(String word, List<String> usedWords) {
+		usedWords.add(word);
+		words.forEach( nextWord -> {
+			if ( nextWord.startsWith(word.substring(word.length() - 1)) 
+					&& !usedWords.contains(nextWord)) {
+				appendNextWord(nextWord, new ArrayList<String>(usedWords));
+			}
+		});
+		
+		if (usedWords.size() >= maxCount) { 
+			maxCount = usedWords.size();
+			String ans = String.join(" - ", usedWords);
+			System.out.println(String.format("[%d]: %s", maxCount, ans ));
+		}
 	}
 }
